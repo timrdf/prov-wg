c93a073db3e469bf89cbb112a77232bca88a1f6c
Tim L <timrdf@gmail.com>
2015-06-26 20:48:06 -0400

    notes on other things to test

--- namespace/bin/test-w3org.sh
+++ namespace/bin/test-w3org.sh
@@ -56,7 +56,10 @@ test_ns 'application/rdf+xml' http://www.w3.org/ns/prov-links  prov-links.owl
 test_ns 'application/xml'     http://www.w3.org/ns/prov-links  prov-links.xsd
 echo
 
+# http://www.w3.org/ns/prov-dc-directmappings.ttl and http://www.w3.org/ns/prov-dc-refinements.ttl
+
 test_ns 'text/turtle'         http://www.w3.org/2011/prov/provenance/prov-o ../provenance/prov-o.ttl
 test_ns 'application/rdf+xml' http://www.w3.org/2011/prov/provenance/prov-o ../provenance/prov-o.rdf
 echo
 
+# The rest of the provenance/ for the other 5+ components
